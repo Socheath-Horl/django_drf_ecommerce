f6b7a7348905b4579468a4967554c5d53bbf5f83
Socheath Horl <horl.socheath@gmail.com>
2023-11-27 14:38:46 +0700

    configuring drf-spectacular schema and swagger

--- drfecommerce/product/views/category.py
+++ drfecommerce/product/views/category.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request: Request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
