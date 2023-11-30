59ec955401529e1414e2ba1d7d431395572924db
Socheath Horl <horl.socheath@gmail.com>
2023-11-30 20:16:16 +0700

    list product by category

--- drfecommerce/product/views/product.py
+++ drfecommerce/product/views/product.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -17,4 +18,12 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request: Request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['get'], detail=False, url_path=r'category/(?P<category>\w+)/all',)
+    def list_product_by_category(self, request: Request, category=None):
+        """"
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
         return Response(serializer.data)
